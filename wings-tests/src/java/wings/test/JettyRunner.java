d43b24edafb0e10e541ec3a471d87d11ade84738
hzeller <hzeller@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2007-10-07 10:21:46 +0000

    o start wings-tests/ and provide a simple way to run the   demos directly from ant.
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings3/trunk@3707 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

---
+++ wings-tests/src/java/wings/test/JettyRunner.java
@@ -0,0 +1,38 @@
+package wings.test;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+/**
+ * Convenience servlet runner to show the demos.
+ */
+public class JettyRunner {
+	public static void main(String argv[]) throws Exception {
+		if (argv.length != 2) { 
+			System.err.println("usage: WingsetRunner <warfile> <port>");
+			System.exit(1);
+		}
+		
+		final String warFile = argv[0];
+		final int port = Integer.parseInt(argv[1]);
+		       
+		Server server = new Server();
+        Connector connector = new SelectChannelConnector();
+        connector.setPort(port);
+        connector.setHost("127.0.0.1");
+        server.addConnector(connector);
+
+        WebAppContext context = new WebAppContext();
+        context.setContextPath("/");
+        context.setWar(warFile);
+        server.setHandler(context);
+        server.setStopAtShutdown(true);
+
+        server.start();
+
+        System.out.println("\n===> Point your browser to "
+		           + " http://localhost:" + port + "/ <===");
+	}
+}
