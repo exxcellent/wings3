5cf4047dd8691d446b64eed77d2734c52fea08ce
hzeller <hzeller@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2007-10-07 12:23:18 +0000

    o add some more small tests.
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings3/trunk@3716 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- wings/src/test/org/wings/plaf/css/UtilsTest.java
+++ wings/src/test/org/wings/plaf/css/UtilsTest.java
@@ -4,9 +4,22 @@ import junit.framework.TestCase;
 
 public class UtilsTest extends TestCase {
 	public void test_escapeJS() {
+		// Generic escapes.
+		assertEquals("\\\\", Utils.escapeJS("\\"));
+		assertEquals("\\b", Utils.escapeJS("\b"));
+		assertEquals("\\f", Utils.escapeJS("\f"));
+		assertEquals("\\n", Utils.escapeJS("\n"));
+		assertEquals("\\r", Utils.escapeJS("\r"));
+		assertEquals("\\t", Utils.escapeJS("\t"));
+		
+		// Special characters are encoded as utf-8 escape
+		assertEquals("\\u0000", Utils.escapeJS("\u0000"));
+		assertEquals("\\u001F", Utils.escapeJS("\u001f"));
+		assertEquals(" ", Utils.escapeJS("\u0020")); // first non-special
+		
+		// and now: all together ;-)
 		assertEquals("foo\\\\\\\"bar", Utils.escapeJS("foo\\\"bar"));
 		assertEquals("\\nfoo\\\\\\\"bar", Utils.escapeJS("\nfoo\\\"bar"));
 		assertEquals("\\nfoo\\\\\\\"bar\\t", Utils.escapeJS("\nfoo\\\"bar\t"));
-		// add more here ;)
 	}
 }
