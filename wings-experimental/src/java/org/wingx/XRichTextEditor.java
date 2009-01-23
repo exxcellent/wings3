2820836f2903715226ca787470ab24b9f0d2299a
florianroks <florianroks@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2009-01-23 01:24:55 +0000

    o fixes a partial reload problem with xrichtexteditor
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings3/trunk@4213 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- wings-experimental/src/java/org/wingx/XRichTextEditor.java
+++ wings-experimental/src/java/org/wingx/XRichTextEditor.java
@@ -1,6 +1,9 @@
 package org.wingx;
 
 import org.wings.*;
+import org.wings.session.SessionManager;
+import org.wings.script.ScriptListener;
+import org.wings.script.JavaScriptListener;
 import org.wings.event.SDocumentEvent;
 import org.wingx.plaf.RichTextEditorCG;
 
@@ -18,6 +21,72 @@ public class XRichTextEditor extends STextComponent {
         super();
         
         this.editorType = editorType;
+
+        final StringBuilder builder = new StringBuilder();
+
+        builder.append("wingS.global.onHeadersLoaded(function() {");
+        String name = "editor_" + getName();
+        String height = "";
+        String width = "";
+        if(getPreferredSize() != null) {
+            height = getPreferredSize().getHeight();
+            width = getPreferredSize().getWidth();
+        } else {
+            height = "100%";
+            width = "100%";
+        }
+        String editor = "SimpleEditor";
+        builder.append("window." + name + " = new YAHOO.widget." + editor + "('" + getName() + "', " +
+                "{" +
+                    "height:\"" + height + "\"," +
+                    "width:\"" + width + "\"," +
+                    "toolbar: {" +
+                        "titlebar: 'Text Editor'," +
+                        "collapse:false,"+
+                        "buttons: ["+
+                            "{ group: 'textstyle', label: 'Font Style',"+
+                                "buttons: ["+
+                                    "{ type: 'push', label: 'Bold', value: 'bold' },"+
+                                    "{ type: 'push', label: 'Italic', value: 'italic' },"+
+                                    "{ type: 'push', label: 'Underline', value: 'underline' },"+
+                                    "{ type: 'separator' },"+
+                                    "{ type: 'select', label: 'Arial', value: 'fontname', disabled: true,"+
+                                        "menu: ["+
+                                            "{ text: 'Arial', checked: true },"+
+                                            "{ text: 'Arial Black' },"+
+                                            "{ text: 'Comic Sans MS' },"+
+                                            "{ text: 'Courier New' },"+
+                                            "{ text: 'Lucida Console' },"+
+                                            "{ text: 'Tahoma' },"+
+                                            "{ text: 'Times New Roman' },"+
+                                            "{ text: 'Trebuchet MS' },"+
+                                            "{ text: 'Verdana' }"+
+                                        "]"+
+                                    "},"+
+                                    "{ type: 'spin', label: '13', value: 'fontsize', range: [ 9, 75 ], disabled: true },"+
+                                    "{ type: 'separator' },"+
+                                    "{ type: 'color', label: 'Font Color', value: 'forecolor', disabled: true },"+
+                                    "{ type: 'color', label: 'Background Color', value: 'backcolor', disabled: true }"+
+                                "]"+
+                            "}"+
+                        "]"+
+                    "}"+
+                "});\n");
+        builder.append(name + ".on('afterNodeChange', function(o) { var elt = document.getElementById('" + getName() + "'); " + name + ".saveHTML(); }, " + name + ", true);");
+        builder.append(name + ".on('editorKeyUp', function(o) { var elt = document.getElementById('" + getName() + "'); " + name + ".saveHTML(); }, " + name + ", true);");
+        builder.append(name + ".on('afterRender', function(o) { var elt = document.getElementById('" + getName() + "'); " + name + ".setEditorHTML('" + getText(this) + "'); }, true);");
+        builder.append(name + ".render();");
+        builder.append("});");
+
+        SessionManager.getSession().getScriptManager().addScriptListener(new JavaScriptListener(null, null, builder.toString()));
+    }
+
+    public String getText(XRichTextEditor component) {
+        String text = component.getText();
+        text = text.replaceAll("\n", "<BR />");
+        text = text.replaceAll("'", "\'");
+
+        return text;
     }
 
     @Override
--- wings-experimental/src/java/org/wingx/plaf/css/RichTextEditorCG.java
+++ wings-experimental/src/java/org/wingx/plaf/css/RichTextEditorCG.java
@@ -2,17 +2,21 @@ package org.wingx.plaf.css;
 
 import org.wingx.XRichTextEditor;
 import org.wings.io.Device;
+import org.wings.io.StringBuilderDevice;
 import org.wings.plaf.Update;
 import org.wings.plaf.css.Utils;
 import org.wings.plaf.css.AbstractUpdate;
 import org.wings.plaf.css.UpdateHandler;
 import org.wings.plaf.css.AbstractComponentCG;
 import org.wings.SComponent;
+import org.wings.SLabel;
+import org.wings.SFrame;
+import org.wings.session.SessionManager;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class RichTextEditorCG extends AbstractComponentCG<XRichTextEditor> implements org.wingx.plaf.RichTextEditorCG<XRichTextEditor> {
-
     public String getText(XRichTextEditor component) {
         String text = component.getText();
         text = text.replaceAll("\n", "<BR />");
@@ -21,71 +25,14 @@ public class RichTextEditorCG extends AbstractComponentCG<XRichTextEditor> imple
         return text;
     }
 
-    public void writeInternal(Device device, XRichTextEditor component) throws IOException {
-        device.print("<textarea");
+    public void writeInternal(Device device, final XRichTextEditor component) throws IOException {
+        device.print("<textarea onload=\"javascript:alert('test');\"");
         Utils.optAttribute(device, "id", component.getName());
         Utils.optAttribute(device, "eid", component.getName());
         Utils.optAttribute(device, "name", component.getName());
         device.print(">");
         device.print(getText(component));
         device.print("</textarea>");
-
-        device.print("<script type=\"text/javascript\">");
-        String name = "editor_" + component.getName();
-
-        String height = "";
-        String width = "";
-        if(component.getPreferredSize() != null) {
-            height = component.getPreferredSize().getHeight();
-            width = component.getPreferredSize().getWidth();
-        } else {
-            height = "100%";
-            width = "100%";
-        }
-
-        String editor = "SimpleEditor";
-
-        device.print("var " + name + " = new YAHOO.widget." + editor + "('" + component.getName() + "', " +
-                "{" +
-                    "height:\"" + height + "\"," +
-                    "width:\"" + width + "\"," +
-                    "toolbar: {" +
-                        "titlebar: 'Text Editor'," +
-                        "collapse:false,"+
-                        "buttons: ["+
-                            "{ group: 'textstyle', label: 'Font Style',"+
-                                "buttons: ["+
-                                    "{ type: 'push', label: 'Bold', value: 'bold' },"+
-                                    "{ type: 'push', label: 'Italic', value: 'italic' },"+
-                                    "{ type: 'push', label: 'Underline', value: 'underline' },"+
-                                    "{ type: 'separator' },"+
-                                    "{ type: 'select', label: 'Arial', value: 'fontname', disabled: true,"+
-                                        "menu: ["+
-                                            "{ text: 'Arial', checked: true },"+
-                                            "{ text: 'Arial Black' },"+
-                                            "{ text: 'Comic Sans MS' },"+
-                                            "{ text: 'Courier New' },"+
-                                            "{ text: 'Lucida Console' },"+
-                                            "{ text: 'Tahoma' },"+
-                                            "{ text: 'Times New Roman' },"+
-                                            "{ text: 'Trebuchet MS' },"+
-                                            "{ text: 'Verdana' }"+
-                                        "]"+
-                                    "},"+
-                                    "{ type: 'spin', label: '13', value: 'fontsize', range: [ 9, 75 ], disabled: true },"+
-                                    "{ type: 'separator' },"+
-                                    "{ type: 'color', label: 'Font Color', value: 'forecolor', disabled: true },"+
-                                    "{ type: 'color', label: 'Background Color', value: 'backcolor', disabled: true }"+
-                                "]"+
-                            "}"+
-                        "]"+
-                    "}"+
-                "});\n");
-        device.print(name + ".on('afterNodeChange', function(o) { var elt = document.getElementById('" + component.getName() + "'); " + name + ".saveHTML(); }, " + name + ", true);");
-        device.print(name + ".on('editorKeyUp', function(o) { var elt = document.getElementById('" + component.getName() + "'); " + name + ".saveHTML(); }, " + name + ", true);");
-        device.print(name + ".on('afterRender', function(o) { var elt = document.getElementById('" + component.getName() + "'); " + name + ".setEditorHTML('" + getText(component) + "'); }, true);");
-        device.print(name + ".render();");
-        device.print("</script>");
     }
 
     public Update getTextUpdate(XRichTextEditor component) {
@@ -104,7 +51,7 @@ public class RichTextEditorCG extends AbstractComponentCG<XRichTextEditor> imple
         public Handler getHandler() {
             UpdateHandler update = new UpdateHandler("runScript");
             String name = "editor_" + component.getName();
-            update.addParameter(name + ".setEditorHTML('" + getText(editor) + "');");
+            update.addParameter("wingS.global.onHeadersLoaded(function() { window." + name + ".setEditorHTML('" + getText(editor) + "'); });");
             // TODO: update size, border, etc.
             return update;
         }
