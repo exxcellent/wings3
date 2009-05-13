7de1ae35470cc5dd9ae2e1d26e24f5ae47587482
florianroks <florianroks@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2009-05-13 15:26:19 +0000

    o refactored TomcatCometWingServlet.java
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings3/trunk@4270 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- wings/src/java/org/wings/comet/TomcatCometWingServlet.java
+++ wings/src/java/org/wings/comet/TomcatCometWingServlet.java
@@ -11,71 +11,77 @@ import java.io.IOException;
 public class TomcatCometWingServlet
         extends CometWingServlet implements CometProcessor {
 
-    public void event(CometEvent cometEvent) throws IOException, ServletException {
+    private void beginEvent(CometEvent cometEvent) throws IOException, ServletException {
+        HttpServletRequest request = cometEvent.getHttpServletRequest();
+        HttpServletResponse response = cometEvent.getHttpServletResponse();
 
-        final HttpServletRequest request = cometEvent.getHttpServletRequest();
-        final HttpServletResponse response = cometEvent.getHttpServletResponse();
+        log.debug("BEGIN: " + request.getSession());
 
-        //cometEvent.setTimeout(TIMEOUT);
+        final Session session = getSession(request);
+        if (session == null) {
+            this.service(request, response);
+            cometEvent.close();
+            return;
+        }
 
-        if (cometEvent.getEventType() == CometEvent.EventType.BEGIN) {
-            log.debug("BEGIN: " + request.getSession());
+        final Comet comet = session.getComet();
+        final Pushable pushable = comet.getPushable();
+        final CometConnectionManager connectionManager = comet.getConnectionManager();
 
-            final Session session = getSession(request);
-            if (session == null) {
-                this.service(request, response);
-                cometEvent.close();
-                return;
-            }
+        final String pathInfo = request.getPathInfo();
 
-            final Comet comet = session.getComet();
-            final Pushable pushable = comet.getPushable();
-            final CometConnectionManager connectionManager = comet.getConnectionManager();
-
-            final String pathInfo = request.getPathInfo();
-
-            if (connectionManager != null) {
-                if (HANGING_PATH.equals(pathInfo)) {
-                    if (!connectionManager.hangingGetActive(true)) {
-                        synchronized (pushable) {
-                            cometEvent.setTimeout((int)comet.getLongPollingTimeout());
-                            pushable.setPushInfo(response);
-                        }
-                    } else {
-                        pushable.setPeriodicPolling(response);
-                        cometEvent.close();
-                    }
-                } else {
-                    final String param = request.getParameter(PERIODIC_POLLING_PARAM);
-                    if (param != null) {
-                        if (!connectionManager.isHangingGetActive()) {
-                            synchronized (pushable) {
-                                if (!pushable.isSwitchActive()) {
-                                    pushable.setSwitchActive(true);
-                                    pushable.switchToHanging();
-                                }
-                            }
-                        }
-                    }
-                    this.service(request, response);
-                    cometEvent.close();
-                }
-            } else {
-                if (HANGING_PATH.equals(pathInfo)) {
+        if (connectionManager != null) {
+            if (HANGING_PATH.equals(pathInfo)) {
+                if (!connectionManager.hangingGetActive(true)) {
                     synchronized (pushable) {
                         cometEvent.setTimeout((int)comet.getLongPollingTimeout());
                         pushable.setPushInfo(response);
                     }
                 } else {
-                    this.service(request, response);
+                    pushable.setPeriodicPolling(response);
                     cometEvent.close();
                 }
+            } else {
+                final String param = request.getParameter(PERIODIC_POLLING_PARAM);
+                if (param != null) {
+                    if (!connectionManager.isHangingGetActive()) {
+                        synchronized (pushable) {
+                            if (!pushable.isSwitchActive()) {
+                                pushable.setSwitchActive(true);
+                                pushable.switchToHanging();
+                            }
+                        }
+                    }
+                }
+                this.service(request, response);
+                cometEvent.close();
             }
+        } else {
+            if (HANGING_PATH.equals(pathInfo)) {
+                synchronized (pushable) {
+                    cometEvent.setTimeout((int)comet.getLongPollingTimeout());
+                    pushable.setPushInfo(response);
+                }
+            } else {
+                this.service(request, response);
+                cometEvent.close();
+            }
+        }
 
-        } else if (cometEvent.getEventType() == CometEvent.EventType.ERROR) {
-            log.error("ERROR: " + request.getSession() + "\n");
+    }
 
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.TIMEOUT) {
+    private void handleError(CometEvent cometEvent) throws IOException, ServletException {
+        HttpServletRequest request = cometEvent.getHttpServletRequest();
+
+        log.error("ERROR: " + request.getSession() + "\n");
+
+        if(cometEvent.getEventSubType() != null) {
+            cometEvent.close();
+            return;
+        }
+
+        switch(cometEvent.getEventSubType()) {
+            case TIMEOUT:
                 log.debug("---Timeout: " + request.getSession() + "\n");
 
                 final Pushable pushable = getPushable(request);
@@ -83,33 +89,63 @@ public class TomcatCometWingServlet
                     pushable.reset();
                     pushable.reconnect();
                 }
-            }
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.CLIENT_DISCONNECT) {
+            break;
+            case CLIENT_DISCONNECT:
                 log.debug("---Client_Disconnect: " + request.getSession() + "\n");
-            }
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.IOEXCEPTION) {
+            break;
+            case IOEXCEPTION:
                 log.debug("---IOException: " + request.getSession() + "\n");
-            }
+            break;
+        }
 
-            cometEvent.close();
+        cometEvent.close();
+    }
 
-        } else if (cometEvent.getEventType() == CometEvent.EventType.END) {
-            log.debug("END: " + request.getSession() + "\n");
+    private void endEvent(CometEvent cometEvent) throws IOException, ServletException {
+        HttpServletRequest request = cometEvent.getHttpServletRequest();
 
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.WEBAPP_RELOAD) {
-                log.debug("---Webapp_Reload: " + request.getSession() + "\n");
-            }
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.SESSION_END) {
-                log.debug("---Session_End: " + request.getSession() + "\n");
-            }
-            if (cometEvent.getEventSubType() == CometEvent.EventSubType.SERVER_SHUTDOWN) {
-                log.debug("---Server_Shutdown: " + request.getSession() + "\n");
-            }
+        log.debug("END: " + request.getSession() + "\n");
+        final Pushable pushable = getPushable(request);
 
-            final Pushable pushable = getPushable(request);
+        if(cometEvent.getEventSubType() == null) {
             pushable.reset();
+            return;
+        }
 
-            cometEvent.close();
+        switch(cometEvent.getEventSubType()) {
+            case WEBAPP_RELOAD:
+                log.debug("---Webapp_Reload: " + request.getSession() + "\n");
+                break;
+            case SESSION_END:
+                log.debug("---Session_End: " + request.getSession() + "\n");
+                break;
+            case SERVER_SHUTDOWN:
+                log.debug("---Server_Shutdown: " + request.getSession() + "\n");
+                break;
+        }
+
+        pushable.reset();
+
+        cometEvent.close();
+    }
+
+    public void event(CometEvent cometEvent) throws IOException, ServletException {
+        //cometEvent.setTimeout(TIMEOUT);
+        if(cometEvent == null || cometEvent.getEventType() == null)
+            return;
+        
+        switch(cometEvent.getEventType()) {
+            case BEGIN:
+                beginEvent(cometEvent);
+            break;
+            case ERROR:
+                handleError(cometEvent);
+            break;
+            case END:
+                endEvent(cometEvent);
+            break;
+            default:
+                break;
         }
     }
 
