3cf4b257174df850c00e20aa5c54cef5c6e4fd60
maldfeld <maldfeld@c8af5dc8-8010-0410-93ac-e0f74e17e1d8>
2009-09-06 08:21:16 +0000

    wings-integration: LocalAdapter cleanup & refactoring
    git-svn-id: https://j-wings.svn.sourceforge.net/svnroot/j-wings/wings3/trunk@4335 c8af5dc8-8010-0410-93ac-e0f74e17e1d8

--- wings-integration/src/java/org/wings/adapter/file/LocalAdapter.java
+++ wings-integration/src/java/org/wings/adapter/file/LocalAdapter.java
@@ -69,10 +69,11 @@ public class LocalAdapter extends AbstractTemplateIntegrationAdapter {
 			}
 		}
     	
-        String url = prepareUrl(integration.getResource().getUrlExtension(null).getReplacedValue(values.toArray(new String[0])));
+        String url = prepareUrl(integration.getResource().getUrlExtension(null).getReplacedValue(values.toArray(new String[values.size()])));
         if (url == null) {
             throw new FileNotFoundException();
         }
+
         return process(getFileContent(new File(url)));
     }
 
@@ -80,39 +81,15 @@ public class LocalAdapter extends AbstractTemplateIntegrationAdapter {
         return getResource(params);
     }
 
-    private String removeBOM(String line) {
-        if (line != null && line.length() > 1) {
-            int codePoint0 = line.codePointAt(0);
-            int codePoint1 = line.codePointAt(1);
-            if (codePoint0 == 0xFEFF) {
-                // utf-16, big-endian
-                return line.substring(1);
-            } else if (codePoint0 == 0xFFFE) {
-                // utf-16, little-endian
-                return line.substring(1);
-            } else if (codePoint0 == 0xEFBB && codePoint1 == 0xBF00) {
-                // utf-8
-                return line.substring(2); // ??? untestet...
-            }
-        }
-        return line;
-    }
-
-    private String getFileContent(File file) throws IOException {
+    protected String getFileContent(File file) throws IOException {
         StringBuilder sb = new StringBuilder();
         
         BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(file), IOUtil.getIOEncoding()));
         try {
             String nl = System.getProperty("line.separator");
-            // read first line
-            String line = input.readLine();
-            if (line != null) {
-                // detect & remove BOM from line 1
-                sb.append(removeBOM(line)).append(nl);
-                // read other lines
-                while ((line = input.readLine()) != null) {
-                    sb.append(line).append(nl);
-                }
+            String line;
+            while ((line = input.readLine()) != null) {
+                sb.append(line).append(nl);
             }
         } finally {
             input.close();
@@ -121,7 +98,6 @@ public class LocalAdapter extends AbstractTemplateIntegrationAdapter {
         return sb.toString();
     }
 
-    @Override
     protected String prepareUrl(String extensionUrl) {
         String baseUrl = integration.getBaseUrl().toExternalForm();
         if (!baseUrl.endsWith("/") || !baseUrl.endsWith("\\")) {
@@ -134,7 +110,6 @@ public class LocalAdapter extends AbstractTemplateIntegrationAdapter {
         return SessionManager.getSession().getServletContext().getRealPath(url);
     }
 
-    @Override
     protected String requestInclude(String url) {
         try {
             return getFileContent(new File(url));
@@ -166,5 +141,4 @@ public class LocalAdapter extends AbstractTemplateIntegrationAdapter {
         }
         return headSource;
     }
-
 }
